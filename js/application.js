70e60c405215776983dd061818798337c4e7e4ce
Maggie Yu <maggieyu@cmu.edu>
2016-03-14 01:29:43 -0400

    started news page, fulbright page done

--- js/application.js
+++ js/application.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 	// Side navbar menu for mobile screens
 	$(".button-collapse").sideNav();
+
+	// Initializing image sliders
+	$('.slider').slider();
+
+	// Initializing material boxes
+	$('.materialboxed').materialbox();
+
 })
